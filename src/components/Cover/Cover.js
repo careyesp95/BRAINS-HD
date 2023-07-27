0b899ba34ccad7d3fb2f0ea2379132bfa0ff9fe3
careyesp95 <careyesp@libertadores.edu.co>
2023-07-27 16:44:20 -0500

    update mobile

--- src/components/Cover/Cover.js
+++ src/components/Cover/Cover.js
@@ -1,11 +1,11 @@
 import React from "react";
-import video from './videoMedia/videos.mp4';
 import "./Cover.css";
 
 const Cover = () => {
+  //<video className="video" src={video} autoPlay loop muted/>
   return (
     <main id='home' className="cover-container">
-      <video className="video" src={video} autoPlay loop muted/>
+      <video className="video" src='./videos.mp4' autoPlay loop muted/>
       <h1>BRAINS HD</h1>
       <p> Conectividad | Velocidad | Estabilidad </p>
     </main>
