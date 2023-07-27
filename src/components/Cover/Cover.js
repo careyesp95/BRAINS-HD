3887374645f7b82d086a693512ce8cfdf4050659
careyesp95 <careyesp@libertadores.edu.co>
2023-07-27 17:14:54 -0500

    update cover

--- src/components/Cover/Cover.js
+++ src/components/Cover/Cover.js
@@ -1,13 +1,13 @@
 import React from "react";
-import ReactPlayer from 'react-player';
 import video from "./videoMedia/videos.mp4";
 import "./Cover.css";
 
 const Cover = () => {
   //<video className="video" src={video} autoPlay loop muted/>
+
   return (
     <main id='home' className="cover-container">
-      <video className="video" src={video} autoPlay loop />
+      <video className="video" src={video} autoPlay loop muted/>
       <h1>BRAINS HD</h1>
       <p> Conectividad | Velocidad | Estabilidad </p>
     </main>
